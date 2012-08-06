41f1ed1b0c2135ede0eb7461b24fe675a8839833
Bill Decker <bill@concreteinteractive.com>
2012-08-05 17:08:07 -0700

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SC2Calculator::Application.config.secret_token = '99e88fd67ceeeef03c8c3bbd5b2248ceaf3c33e364a76196148663e0a7b35413d3a2143e8c4232b01d22fd1aad5b0f114058f909af75ded71e88cc29d372845c'
